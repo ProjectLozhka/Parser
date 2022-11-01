9b92a308c58849a85ba7c3947f23b26f8ac95149
Dan-K <dvkostikov@edu.hse.ru>
2022-11-02 00:13:26 +0300

    test

--- main.py
+++ main.py
@@ -6,17 +6,10 @@ import csv
 from fake_useragent import UserAgent
 import random
 
+
 def pars_title(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    ua = UserAgent()
-    hdr = {'User-Agent': ua.random,
-           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-           'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-           'Accept-Encoding': 'none',
-           'Accept-Language': 'en-US,en;q=0.8',
-           'Connection': 'keep-alive'}
-    response = requests.get(url, headers=hdr)
+    proxies, hdr = proxies_headers()
+    response = requests.get(url, headers=hdr, proxies=proxies)
     soup = BeautifulSoup(response.text, 'html.parser')
     title = ''
     quotes = soup.find_all('h1', class_='title mathjax')
@@ -35,15 +28,17 @@ def pars_title(url):
     res = [title, annotation, pdf]
     return res
 
+
 def pars_for_month_in_year(url, writer):
+    proxies, hdr = proxies_headers()
     n = 1
-    response = requests.get(url)
+    response = requests.get(url, proxies=proxies, headers=hdr)
     soup = BeautifulSoup(response.text, 'html.parser')
     quotes = soup.find_all('a', title='Abstract')
     base_url = 'https://arxiv.org'
     for quote in quotes:
         try:
-            row = pars_title(base_url+quote['href'])
+            row = pars_title(base_url + quote['href'])
             print('Wrote row: ' + str(n))
             writer.writerow(row)
             n += 1
@@ -51,19 +46,23 @@ def pars_for_month_in_year(url, writer):
             print('Error1')
     pass
 
+
 def take_url_for_all(url):
-    response = requests.get(url)
+    proxies, hdr = proxies_headers()
+    response = requests.get(url, proxies=proxies, headers=hdr)
     base_url = 'https://arxiv.org'
     soup = BeautifulSoup(response.text, 'html.parser')
     quotes = soup.find_all('small')
     n = len(quotes)
-    quote = quotes[n-3].find_all('a')
+    quote = quotes[n - 3].find_all('a')
     n = len(quote)
-    res = base_url + quote[n-1]['href']
+    res = base_url + quote[n - 1]['href']
     return res
 
+
 def pars_for_months(url):
-    response = requests.get(url)
+    proxies, hdr = proxies_headers()
+    response = requests.get(url, headers=hdr, proxies=proxies)
     base_url = 'https://arxiv.org'
     soup = BeautifulSoup(response.text, 'html.parser')
     quotes = soup.find('ul')
@@ -71,11 +70,13 @@ def pars_for_months(url):
     res = []
     for quote in quotes:
         quote_1 = quote.find('a')
-        res.append(base_url+quote_1['href'])
+        res.append(base_url + quote_1['href'])
     return res
 
+
 def pars_for_years(url):
-    response = requests.get(url)
+    proxies, hdr = proxies_headers()
+    response = requests.get(url, proxies=proxies, headers=hdr)
     base_url = 'https://arxiv.org'
     soup = BeautifulSoup(response.text, 'html.parser')
     quotes = soup.find_all('ul')
@@ -83,8 +84,9 @@ def pars_for_years(url):
     res = []
     quotes = quotes[7].find_all('a')
     for quote in quotes:
-        res.append(base_url+quote['href'])
+        res.append(base_url + quote['href'])
     return res
+
 
 def main_parse_category_bez_sveniy(url, filename):
     f = open(filename, 'w')
@@ -92,10 +94,12 @@ def main_parse_category_bez_sveniy(url, filename):
     header = ['Ttile', 'Annotation', 'PDF']
     writer.writerow(header)
     years = pars_for_years(url)
+    random.shuffle(years)
     for year in years:
         print('Parsing year: ' + year)
         try:
             months = pars_for_months(year)
+            random.shuffle(months)
             for month in months:
                 try:
                     print('Parsing month: ' + month)
@@ -108,9 +112,32 @@ def main_parse_category_bez_sveniy(url, filename):
     f.close()
     pass
 
-main_parse_category_bez_sveniy('https://arxiv.org/archive/astro-ph', 'astrophysics.csv')
-#print(pars_for_years('https://arxiv.org/archive/astro-ph'))
-#print(pars_for_months('https://arxiv.org/year/astro-ph/22'))
-#print(take_url_for_all('https://arxiv.org/list/astro-ph/2201'))
-#print(pars_for_month_in_year('https://arxiv.org/list/astro-ph/2201?show=1291'))
-#print(pars_title('https://arxiv.org/abs/2201.00021'))
+def proxies_headers():
+    ua = UserAgent()
+    hdr = {'User-Agent': ua.random,
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+           'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+           'Accept-Encoding': 'none',
+           'Accept-Language': 'en-US,en;q=0.8',
+           'Connection': 'keep-alive'}
+    proxi = ['185.40.76.9:25739',
+             '95.181.163.15:25739',
+             '62.76.142.8:25739',
+             '46.8.158.186:25739',
+             '194.147.114.3:25739',
+             '31.184.240.129:25739',
+             '46.161.41.148:25739',
+             '45.8.125.193:25739',
+             '93.189.47.89:25739',
+             '93.189.40.69:25739',
+             ]
+    proxy2 = 'http://uKZtU5TLAVBj:dan-kos1@' + random.choice(proxi)
+    proxies = {'http': proxy2}
+    return proxies, hdr
+
+main_parse_category_bez_sveniy('https://arxiv.org/archive/astro-ph', 'astrophysics1.csv')
+# print(pars_for_years('https://arxiv.org/archive/astro-ph'))
+# print(pars_for_months('https://arxiv.org/year/astro-ph/22'))
+# print(take_url_for_all('https://arxiv.org/list/astro-ph/2201'))
+# print(pars_for_month_in_year('https://arxiv.org/list/astro-ph/2201?show=1291'))
+# print(pars_title('https://arxiv.org/abs/2201.00021'))
--- proxi.py
+++ proxi.py
@@ -1,3 +1,5 @@
+import random
+
 from lxml.html import fromstring
 import requests
 from itertools import cycle
@@ -19,18 +21,40 @@ def get_proxies():
 # If you are copying and pasting proxy ips, put in the list below proxies = ['121.129.127.209:80',
 # '124.41.215.238:45169', '185.93.3.123:8080', '194.182.64.67:3128', '106.0.38.174:8080', '163.172.175.210:3128',
 # '13.92.196.150:8080']
-proxies = get_proxies()
-proxy_pool = cycle(proxies)
-url = 'https://httpbin.org/ip'
-for i in range(1, 11):
-    # Get a proxy from the pool
-    proxy = next(proxy_pool)
-    print("Request #%d" % i)
-    try:
-        response = requests.get(url, proxies={"http": proxy, "https": proxy})
-        print(response.json())
-    except:
-        # Most free proxies will often get connection errors. You will have retry the entire request using another
-        # proxy to work. We will just skip retries as it's beyond the scope of this tutorial, and we are only
-        # downloading a single url
-        print("Skipping. Connnection error")
+def proxies_array():
+    proxies = get_proxies()
+    proxy_pool = cycle(proxies)
+    url = 'https://httpbin.org/ip'
+    res = []
+    for i in range(1, 11):
+        # Get a proxy from the pool
+        proxy = next(proxy_pool)
+        print("Request #%d" % i)
+        try:
+            response = requests.get(url, proxies={"http": proxy, "https": proxy})
+            print(response.json())
+            res.append(proxy)
+        except:
+            # Most free proxies will often get connection errors. You will have retry the entire request using another
+            # proxy to work. We will just skip retries as it's beyond the scope of this tutorial, and we are only
+            # downloading a single url
+            print("Skipping. Connnection error")
+    return res
+
+
+# print(proxies_array())
+proxi = ['185.40.76.9:25739',
+         '95.181.163.15:25739',
+         '62.76.142.8:25739',
+         '46.8.158.186:25739',
+         '194.147.114.3:25739',
+         '31.184.240.129:25739',
+         '46.161.41.148:25739',
+         '45.8.125.193:25739',
+         '93.189.47.89:25739',
+         '93.189.40.69:25739',
+         ]
+proxy2 = 'http://uKZtU5TLAVBj:dan-kos1@' + random.choice(proxi)
+proxies = {'http': proxy2}
+response = requests.get('http://httpbin.org/ip', proxies=proxies)
+print(response.text)
